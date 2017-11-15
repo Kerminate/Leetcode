dff1a6d5837addb723d203d32ac074332f809ee6
Kerminate <kepeilin@outlook.com>
2017-11-15 22:36:43 +0800

    1

---
+++ 001/main.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  var res = []
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        res.push(i)
+        res.push(j)
+        // console.log(res)
+        return res
+      }
+    }
+  }
+}
+
+// var nums = [2, 7, 11, 15], target = 9
+// twoSum(nums, target)
