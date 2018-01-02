9d6cee2cb245e4f9d4c776d24e2665b58694faac
Kerminate <kepeilin@outlook.com>
2018-01-02 18:43:11 +0800

    013

---
+++ 013/main.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const rom = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+  let sum = 0
+  for (let i = 0; i < s.length - 1; i++) {
+    if (rom[s[i]] < rom[s[i + 1]]) {
+      sum -= rom[s[i]]
+    } else {
+      sum += rom[s[i]]
+    }
+  }
+  sum += rom[s[s.length - 1]]
+  return sum
+}
+
+console.log(romanToInt('XCIX'))
