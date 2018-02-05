203183ca29ac554903665a28ab4534bdab422789
Kerminate <kepeilin@outlook.com>
2018-02-05 21:20:04 +0800

    051 main.js

---
+++ 051/main.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+  const col = Array.from({length: n}).map(() => 0)
+  const diag1 = Array.from({ length: 2 * n - 1 }).map(() => 0)
+  const diag2 = Array.from({ length: 2 * n - 1 }).map(() => 0)
+  const board = Array.from({ length: n }).map(() => Array.from({ length: n }, () => '.'))
+  const res = []
+  dfs(0)
+  return res
+
+  function dfs (y) {
+    if (y === n) {
+      let arr = board.slice()
+      let str = []
+      for (let i = 0; i < arr.length; i++) {
+        str.push(arr[i].join(''))
+      }
+      res.push(str)
+      return
+    }
+    for (let i = 0; i < n; i++) {
+      if (col[i] || diag1[i + y] || diag2[i - y + n - 1]) continue
+      col[i] = 1
+      diag1[i + y] = 1
+      diag2[i - y + n - 1] = 1
+      board[y][i] = 'Q'
+      dfs(y + 1)
+      col[i] = 0
+      diag1[i + y] = 0
+      diag2[i - y + n - 1] = 0
+      board[y][i] = '.'
+    }
+  }
+}
+
+console.log(solveNQueens(4))
+// solveNQueens(8)
